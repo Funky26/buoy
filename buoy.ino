991585669b77c207c3ad4fde7772284ddde8700c
Jakub Jelínek <funkyfunjj@gmail.com>
2024-12-22 23:14:27 +0100

    pressure

--- buoy.ino
+++ buoy.ino
@@ -1,5 +1,9 @@
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
 
 unsigned long timeSaved = 0;
+
+
 //sensor for detecting water in buoy(sensor for water surface)
 //S  analog
 //+  power(digital pin)
@@ -7,13 +11,24 @@ unsigned long timeSaved = 0;
 const int waterSensorRead = 34;
 const int waterSensorPower = 25;
 bool isWater= false;
-int waterLevel;
+int waterLevel;//not used
+
+//pressure sensor
+//vcc  power
+//gnd - ground
+//scl (must be I2C) 22
+//sda  (must be I2C) 21
+
+Adafruit_BMP085 bmp180;
+int correction = 32;
+
 void setup() {
   Serial.begin(115200);
 
   // set up of water detecting sensor
-  
   pinMode(waterSensorPower, OUTPUT);
+  //setup for pressure sensor
+  bmp180.begin();
 
   delay(1000);
 }
@@ -27,6 +42,17 @@ void loop() {
     Serial.print("Water in buoy:");
     checkForWater();
 
+    //temperature above the water
+    Serial.print("Temperature(above the water): ");
+    Serial.print(bmp180.readTemperature());
+    Serial.println("°C");
+
+    //atmospheric pressure
+    Serial.print((bmp180.readPressure() + correction*100)/100.00);
+    Serial.println(" hPa");
+
+    Serial.println("-------------------------------");
+    delay(1000);
     timeSaved = millis();
   }
 }
